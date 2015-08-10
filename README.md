49e8a5dc6423c233f68d07616e97069d068c8e02
codekraft-studio <info@codekraft.it>
2015-08-10 10:03:09 +0200

    added application files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# angular-image-preview
-an angularjs directive that create preview for your input file types
+# angular-file-preview
+### display your images before upload it
+you will never upload the wrong image again!
+
+Simply copy and paste the directive to use it in your application.
+
+```javascript
+angular.module('myApp', []).
+  directive(...)
+```
+and add to your html template the directive in every input file where you want to use it:
+
+```html
+<input type="file" ng-model="myUpload" file-preview>
+```
+
+it works with multiple files too:
+```html
+<input type="file" ng-model="myUpload" multiple file-preview>
+```
+
+The directive takes care to update your model value, too!
+
+Developed by Filippo Conti - [codekraft-studio](http://codekraft.it)
---
+++ src/filepreview.directive.js
@@ -0,0 +1,106 @@
+angular.module('app').
+
+  directive('imagePreview', imagePreview);
+
+  function imagePreview() {
+
+    var directive = {
+      restrict: 'A',
+      require: 'ngModel',
+      link: link
+    }
+
+    return directive;
+
+    function link(scope, element, attrs, ngModel) {
+
+      var id = attrs.ngModel + '-preview';
+
+      // create file reader
+      var reader = new FileReader();
+
+      // create preview image element
+      function createPreview(src) {
+        var img = angular.element( document.createElement('img') );
+        img[0].src = src;
+        img[0].className = 'preview';
+        return img[0];
+      }
+
+      // check for child nodes
+      function hasNodes(element) {
+        return (element.childNodes.length);
+      }
+
+      // during change event
+      element.on('change', function(e) {
+
+        // set model view value
+        attrs.multiple ? ngModel.$setViewValue(files) : ngModel.$setViewValue(files[0]);
+
+        // get element if present
+        var container = angular.element( document.getElementById(id) );
+        // reset item count
+        var count = 0;
+        // get files
+        var files = element[0].files;
+
+        reader.onload = function(e) {
+
+          scope.$apply(function(){
+
+            if( attrs.accept && attrs.accept.indexOf('image/*') > -1 ) {
+
+              container[0].appendChild( createPreview(e.target.result) );
+
+            } else {
+
+              if( e.target.result.indexOf('data:image') > -1 ) {
+                container[0].appendChild( createPreview(e.target.result) );
+              }
+
+            }
+
+          })
+
+        }
+
+        reader.onloadend = function() {
+          scope.$apply(function() {
+            if( count < (files.length-1) ) {
+              // increase count
+              count++;
+              // read next file
+              return reader.readAsDataURL( files[count] );
+            }
+          })
+        }
+
+        // check if there are files to read
+        if( files && files.length ) {
+
+          // add preview container if is not present
+          if( !container.length ) {
+            container = angular.element( document.createElement('div') );
+            container[0].id = id;
+            element[0].parentNode.insertBefore(container[0], element[0].nextSibling);
+          }
+
+          // reset container
+          container[0].innerHTML = '';
+
+          // read element data url
+          reader.readAsDataURL( files[count] );
+
+        } else {
+
+          if( hasNodes( container[0] ) ) {
+            container[0].innerHTML = '';
+          }
+
+        }
+      })
+
+    }
+
+  }
