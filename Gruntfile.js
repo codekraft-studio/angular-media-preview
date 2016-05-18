64d22a2c50f3206f31d8cb67374f2ba04abd5378
codekraft-studio <info@codekraft.it>
2016-05-18 14:32:52 +0200

    added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      dist: {
+        src: ['src/<%= pkg.name %>.module.js', 'src/<%= pkg.name %>.*.js'],
+        dest: 'dist/<%= pkg.name %>.js',
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['concat:dist', 'uglify:dist']);
+
+}
