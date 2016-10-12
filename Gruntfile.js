26961d3b9f7d18ffd88f87a4107fd6760909261a
codekraft-studio <info@codekraft.it>
2016-10-12 23:47:54 +0200

    added watch task and project banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,57 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-    concat: {
-      dist: {
-        src: ['src/<%= pkg.name %>.module.js', 'src/<%= pkg.name %>.*.js'],
-        dest: 'dist/<%= pkg.name %>.js',
-      }
-    },
+        concat: {
+            options: {
+                banner: '/**\n' +
+                        '* Package: <%= pkg.name %> - v<%= pkg.version %> \n' +
+                        '* Description: <%= pkg.description %> \n' +
+        				'* Last build: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+						'* @author <%= pkg.author %> \n' +
+						'* @license <%= pkg.license %> \n'+
+                        '*/\n'
+            },
+            dist: {
+                src: ['src/<%= pkg.name %>.module.js', 'src/<%= pkg.name %>.*.js'],
+                dest: 'dist/<%= pkg.name %>.js',
+            }
+        },
 
-    uglify: {
-      options: {
-        mangle: false
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+        uglify: {
+            options: {
+                mangle: false,
+                banner: '/**\n' +
+                        '* Package: <%= pkg.name %> - v<%= pkg.version %> \n' +
+                        '* Description: <%= pkg.description %> \n' +
+        				'* Last build: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+						'* @author <%= pkg.author %> \n' +
+						'* @license <%= pkg.license %> \n'+
+                        '*/\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['src/*.js'],
+                tasks: ['build']
+            }
         }
-      }
-    }
 
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['concat:dist', 'uglify:dist']);
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['concat', 'uglify']);
 
 }
